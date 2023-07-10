9c06df07c9b84a7335b8e9e892efe9f4fb4f7e66
Yannis <kalfasyan@gmail.com>
2023-07-10 17:04:06 +0200

    new release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='plakakia',
-    version='0.1.0',
+    version='0.2.0',
     author='Yannis Kalfas',
     author_email='kalfasyan@gmail.com',
     description='Python image tiling library for image processing, object detection, etc.',
